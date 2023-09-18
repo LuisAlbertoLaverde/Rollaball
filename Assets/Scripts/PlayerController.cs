f08d65441fbe41ed3657f07d8c138c11a10b9d84
Alberto <unumnio@gmail.com>
2023-09-18 06:16:15 -0500

    detecting collisions whit Collectibles, displaying score and text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,27 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 0;
+    [SerializeField] private TextMeshProUGUI _countText;
+    [SerializeField] private GameObject _winTextObject;
 
     private Rigidbody _rb;
+    private int _count;
     private float _movementX;
     private float _movementY;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        _count = 0;
+        SetCountText();
+        _winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -22,10 +32,30 @@ public class PlayerController : MonoBehaviour
         _movementY = movementVector.y;
     }
 
+    private void SetCountText()
+    {
+        _countText.text = $"Count: {_count.ToString()}";
+        if (_count >= 12)
+        {
+            _winTextObject.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
 
         _rb.AddForce(movement * _speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+
+            SetCountText();
+        }
     }
 }
